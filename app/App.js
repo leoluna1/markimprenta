8c7647d1be8e1eb755f4092a8793700977ef1a55
Leito <leito@MacBook-Air-de-Leito.local>
2026-07-01 20:36:54 -0500

    Remove FAQ section

--- app/App.js
+++ app/App.js
@@ -7,7 +7,6 @@ import CatalogController    from './controllers/CatalogController.js';
 import QuoteController      from './controllers/QuoteController.js';
 import ContactController    from './controllers/ContactController.js';
 import HeroView             from './views/HeroView.js';
-import FaqView              from './views/FaqView.js';
 import SettingsController   from './controllers/SettingsController.js';
 import ReviewsController    from './controllers/ReviewsController.js';
 import PortfolioController  from './controllers/PortfolioController.js';
@@ -18,7 +17,6 @@ document.addEventListener('DOMContentLoaded', async () => {
   new CatalogController().init();
   await new QuoteController().init();
   new ContactController().init();
-  new FaqView().bind();
   new SettingsController().init();
   new ReviewsController().init();
   new PortfolioController().init();
